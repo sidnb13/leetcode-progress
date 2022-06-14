2ec972976b7fb6b07294fe48c32c82a8e34a9f7e
Sidharth Baskaran <sidnbaskaran@gmail.com>
2022-06-14 17:02:01 -0500

    Add cred auth for google

--- leetcode-tool.py
+++ leetcode-tool.py
@@ -164,8 +164,32 @@ class LeetcodeDataSourcer:
         pass
     
 class SpreadsheetUpdater:
-    pass
+    def __init__(self) -> None:
+        self.SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+        self.SPREADSHEET_ID = '1QPPbdOEkY3lVqNHcejLFSL2Q7nIqxBYoOliPTk5l1eE'
+        
+    def get_creds(self):
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', self.SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+        
+        return creds
+    
 
 if __name__ == '__main__':
-    lc = LeetcodeDataSourcer()
-    lc.get_submissions()
+    su = SpreadsheetUpdater()
+    su.get_creds()
